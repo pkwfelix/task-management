c197e1b6ae259dd4e7b95c07bb483c78e6684dc5
Felix Phang <pkwfelix@gmail.com>
2023-10-16 20:55:27 +0800

    Update vuetify to 3.3.4

--- src/views/TaskBoard.vue
+++ src/views/TaskBoard.vue
@@ -1,10 +1,12 @@
 <template>
 	<TaskProgress />
+	<TaskAction />
 	<TaskGrid />
 	<TaskModal />
 </template>
 <script setup>
 import TaskProgress from '@/components/TaskProgress.vue'
+import TaskAction from '@/components/TaskAction.vue'
 import TaskGrid from '@/components/TaskGrid.vue'
 import TaskModal from '@/components/TaskModal.vue'
 </script>

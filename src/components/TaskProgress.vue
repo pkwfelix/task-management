264557dda3b86d9b61ba56c3c57906c03299719f
Felix Phang <pkwfelix@gmail.com>
2023-10-17 23:10:33 +0800

    Touch up Progress

--- src/components/TaskProgress.vue
+++ src/components/TaskProgress.vue
@@ -1,22 +1,25 @@
 <template>
     <v-container with-background>
-        <v-progress-linear model-value="20" :height="12"></v-progress-linear>
-        {{ progressMsg }}
+        <v-progress-linear 
+            v-model="progress.done" 
+            :height="25"
+            rounded>
+            <template v-slot:default="{ value }">
+                <strong>{{ Math.ceil(value) }}%</strong>
+            </template>
+        </v-progress-linear>
     </v-container>
 </template>
 
-<script>
-export default {
-    setup () {
-        
-        const progressMsg = 'progressMsg message'    
-        return {
-            progressMsg
-        }
+<script setup>
+import { useTaskStore } from '@/store/task';
+const taskStore = useTaskStore();
+import { computed } from 'vue';
+
+const progress = computed(() => {
+    return {
+        total: taskStore.tasks.length,
+        done: taskStore.tasks.filter(task => task.status == 'done').length / taskStore.tasks.length * 100,
     }
-}
+});
 </script>
-
-<style lang="scss" scoped>
-
-</style>
